913ea833302fa9e8e17ab34d3eb71ce25ec68140
archangel2006 <vaibhavideep26@gmail.com>
2026-07-05 01:36:02 +0530

    fix CORS error

--- Backend/app.py
+++ Backend/app.py
@@ -22,7 +22,7 @@ app = FastAPI(title="AI Recipe Assistant")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
